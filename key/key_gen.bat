be0cadad0f58cc3f1dd06c5690ca93c93fd99187
changxiangzhong <chang@hyper.no>
2015-05-01 14:16:43 +0200

    Add script to generate a root CA
    If this CA is trusted by cellphone/desktop, any certificates signed by this one would be trusted as well

--- key/key_gen.bat
+++ key/key_gen.bat
@@ -15,3 +15,28 @@ keytool -exportcert -alias serverkey -file server-public.cer -keystore server.jk
 
 # PKCS12 keystore is commonly used in Windows & Android.
 keytool -importkeystore -srckeystore chang.jks -srcstoretype JKS -srcstorepass password -destkeystore chang.pfx -deststoretype PKCS12 -deststorepass password
+
+=========================Script to generated a CA, use the CA to provision the key====================================
+
+# Create a root CA
+openssl genrsa -out rootCA.key 4096
+openssl req -x509 -new -nodes -key rootCA.key -days 3650 -out rootCA.pem.crt
+
+# import this rootCA.pem.crt As a root CA to Android device
+
+# Generate keystore
+keytool -genkey -alias tomcat -keyalg RSA -keystore server.jks
+
+# Generate a certificate signing request (a.k.a. CSR)
+keytool -certreq -alias tomcat -keystore tomcat.jks -file tomcat.csr
+
+# Sign the CSR with root CA
+openssl x509 -req -in tomcat.csr -CA ~/key/key3-ca/chang-root-CA.pem.crt -CAkey ~/key/key3-ca/chang-root-CA.key -CAcreateserial -out tomcat.pem.crt -days 3650
+
+# Import the ROOT/Intermediate trusted CA certificate
+keytool -import -trustcacerts -alias root -file ~/key/key3-ca/chang-root-CA.pem.crt -keystore tomcat.jks
+
+# Then we can imported the Certificate by our own root CA
+keytool -import -trustcacerts -alias tomcat -file tomcat.pem.crt -keystore tomcat.jks
+
+# All done! enjoy!
